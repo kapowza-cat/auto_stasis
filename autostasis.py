85b3089c6be9426b25b1a3676e50cbaf4780b0fc
Ryan <ryan.a.ellis7@gmail.com>
2026-01-04 20:46:45 -0700

    can read

---
+++ autostasis.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+
+while True:
+    x, y = pyautogui.position()
+    r,g,b = pyautogui.pixel(x, y)
+    print(r,g,b)
+    time.sleep(0.1)
---
+++ finder.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+
+
+while True:
+    mousex, mousey = pyautogui.position()
+    print(mousex, " ", mousey)
+    time.sleep(0.1)
